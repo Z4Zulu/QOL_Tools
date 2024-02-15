7ab5f2a2c33c07a868fe8eb7f160271112fc6ed2
Z4Zulu <93502855+Z4Zulu@users.noreply.github.com>
2024-02-14 19:37:26 -0500

    Delete ScanRef.sh

--- ScanRef.sh
+++
@@ -1,9 +0,0 @@
-==============================Scanning==========================================
-
-===RustScan====
-rustscan -a $IP -n --ulimit 70000 -t 5000 -- -A -Pn
-
-===Nmap====
-nmap -p- -sT -sV -A $IP
-nmap -p- --script=vuln $IP
-###  --script smb-enum-shares
