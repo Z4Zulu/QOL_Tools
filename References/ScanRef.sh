e8361ca2066c78766e49c05b83fb0940f4cc76d0
Z4Zulu <93502855+Z4Zulu@users.noreply.github.com>
2024-02-14 19:37:12 -0500

    Create ScanRef.sh

---
+++ References/ScanRef.sh
@@ -0,0 +1,9 @@
+==============================Scanning==========================================
+
+===RustScan====
+rustscan -a $IP -n --ulimit 70000 -t 5000 -- -A -Pn
+
+===Nmap====
+nmap -p- -sT -sV -A $IP
+nmap -p- --script=vuln $IP
+###  --script smb-enum-shares
